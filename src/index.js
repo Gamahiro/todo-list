847eac30f90a894cb1c80f16552e353cf6a3fa6c
Johan <jpettersen50@gmail.com>
2022-09-05 14:14:38 +0200

    yes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './style.css';
+
+const defaultProject = [];
+
+const taskFactory = (taskTitle, taskDescription, taskPriority, taskTimeDate) => {
+
+    return { taskTitle, taskDescription, taskPriority, taskTimeDate };
+
+};
+
+
+function createTask(taskTitle, taskDescription, taskPriority, taskTimeDate, project) {
+
+    let newTask = taskFactory(taskTitle, taskDescription, taskPriority, taskTimeDate);
+
+    if (project == undefined) {
+        project = defaultProject;
+    }
+    project.push(newTask);
+    console.log(project);
+}
+
+createTask('Dentist', 'At 123 road', '1', '2pm');
+
